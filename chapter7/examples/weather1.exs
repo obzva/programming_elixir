e1b81afa7cf2fa710a4411509929eb573ceec95b
obzva <dngyng1000@gmail.com>
2023-10-11 00:15:30 +0900

    fixed error in example code files

--- chapter7/examples/weather1.exs
+++ chapter7/examples/weather1.exs
@@ -1,6 +1,6 @@
 defmodule WeatherHistory do
   def for_location([], _target_loc), do: []
-  def for_location([timestamp, target_loc, temperature, rainfall | tail], target_loc) do
+  def for_location([[timestamp, target_loc, temperature, rainfall] | tail], target_loc) do
     [[timestamp, target_loc, temperature, rainfall] | for_location(tail, target_loc)]
   end
   def for_location([ _ | tail], target_loc), do: for_location(tail, target_loc)
