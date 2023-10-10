ef3a4a3d8a66660d9d2354a17cfa19d033257bfb
obzva <dngyng1000@gmail.com>
2023-10-11 00:25:28 +0900

    modified exercise code

--- chapter7/exercises/exercise7_4.exs
+++ chapter7/exercises/exercise7_4.exs
@@ -1,5 +1,5 @@
 defmodule MyList do
-  def span(from, to) when from > to, do: raise "from should be less than or equal to to"
+  def span(from, to) when from > to, do: raise "'from' should be less than or equal to 'to'"
   def span(from, from), do: [from]
   def span(from, to), do: [from | span(from + 1, to)]
 end
